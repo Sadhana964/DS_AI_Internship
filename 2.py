140bfbf8932836b8fe23ad547e8045bd77c19f50
Sadhana964 <sadhanahugar1309@gmail.com>
2026-02-04 22:26:38 +0530

    initial commit

---
+++ 2.py
@@ -0,0 +1,22 @@
+'''inventory = ["Apples", "Bananas", "Carrots", "Dates"]
+print("Current Inventory:", inventory)
+inventory.append("Eggs")
+inventory.remove("Bananas")
+inventory.sort()
+print("Final Updated Inventory:", inventory)
+'''
+
+'''temperatures = [22, 24, 25, 28, 30, 29, 27, 26, 24, 22]
+print("First Reading:", temperatures[0])
+print("Last Reading:", temperatures[-1])
+afternoon_peak = temperatures[3:6]
+print("Afternoon Peak Readings:", afternoon_peak)
+last_three_hours = temperatures[-3:]
+print("Last 3 Hours:", last_three_hours)
+'''
+
+
+
+screen_res = (1920, 1080)
+print(f"Current Resolution: {screen_res[0]}x{screen_res[1]}")
+print("Tuples cannot be modified!")
